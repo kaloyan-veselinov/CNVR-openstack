cdac19ee38e2978a4cc81b06dc62971d492a2f2f
Kaloyan Veselinov <kvesselinov@gmail.com>
2019-01-16 22:39:32 +0100

    Removing useless command from postgress install script

--- install_postgres.sh
+++ install_postgres.sh
@@ -1,5 +1,4 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
-sudo -i -u postgres -c
 sudo -u postgres psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
 sudo -u postgres psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
