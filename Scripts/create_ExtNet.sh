747a9b2a065eb6c7dad189a400399150b39eb318
kaloyan.veselinov <kaloyan.veselinov@l090.lab.dit.upm.es>
2019-01-23 11:17:30 +0100

    Reorganisation fichiers

---
+++ Scripts/create_ExtNet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+openstack network create --share --external --provider-physical-network provider --provider-network-type flat ExtNet
+openstack subnet create --network ExtNet --gateway 10.0.10.1 --dns-nameserver 10.0.10.1 --subnet-range 10.0.10.0/24 --allocation-pool start=10.0.10.100,end=10.0.10.200 ExtSubNet
