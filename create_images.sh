0395620020d29e45b0b84752444500b8b4d5a5a8
kaloyan.veselinov <kaloyan.veselinov@l090.lab.dit.upm.es>
2019-01-23 11:18:59 +0100

    MAJ reorganisation

--- create_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# usage: create_images.sh <vm-web_id> <vm-postgres_id>
-
-openstack server image create $1 
-openstack server image create $2
-openstack image list
--- init_openstack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# usage: init_openstack.sh <external interface>
-
-/mnt/vnx/repo/cnvr/bin/get-openstack-tutorial.sh
-cd /mnt/tmp/openstack_lab-ocata_4n_classic_ovs-v04
-sudo vnx -f openstack_lab.xml -t
-sudo vnx -f openstack_lab.xml -x start-all,load-img
-sudo vnx_config_nat ExtNet $1
