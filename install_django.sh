363a7cd1050f7ed8ca06b71a25a6b5a2767a136f
kaloyan.veselinov <kaloyan.veselinov@l090.lab.dit.upm.es>
2019-01-23 09:43:44 +0100

    Suppression scripts inutiles

--- install_django.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install python3-pip
-sudo pip3 install Django psycopg2
-git clone https://github.com/kaloyan-veselinov/CNVR_django_demo.git
--- install_postgres.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt install postgresql postgresql-contrib
-sudo -u postgres psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
-sudo -u postgres psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
